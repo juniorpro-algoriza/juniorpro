1b248e540585873e6852e410e6f787a70e44e726
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-07-10 17:58:43 +0300

    add syle page:

---
+++ app/(pages)/style/button/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@headlessui/react";
+
+const ButtonStylePage = () => {
+  return (
+    <main>
+      <Button>Button</Button>
+    </main>
+  );
+};
+
+export default ButtonStylePage;
