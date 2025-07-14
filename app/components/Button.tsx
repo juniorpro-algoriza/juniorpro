6435c2960a6bb4b98207658f352ce1f11b074bf2
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-14 19:25:15 +0300

    update button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -49,7 +49,7 @@ export const Button = ({
 };
 
 const baseStyles = `
-  inline-flex items-center justify-center
+  inline-flex items-center justify-center hover:cursor-pointer
   font-medium rounded-lg transition-all duration-200
   focus:outline-none focus:ring-2 focus:ring-offset-2
   disabled:opacity-50 disabled:cursor-not-allowed
