a5cc063df791d756519c1f6294e15787e72fc566
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-14 19:06:42 +0300

    update input style page

--- app/(pages)/style/input/page.tsx
+++ app/(pages)/style/input/page.tsx
@@ -1,16 +1,18 @@
 import { Input } from '@components';
+import { BiDollar, BiSearch } from 'react-icons/bi';
 
 const InputStylePage = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
       <Input label='Name' placeholder='Enter your name' />
       <Input label='Email' type='email' placeholder='Enter your email' />
+      <Input label='Search' placeholder='Search...' leftIcon={<BiSearch />} />
+      <Input label='Amount' placeholder='0.00' rightIcon={<BiDollar />} />
       <Input
         label='Password'
         type='password'
         placeholder='Enter your password'
       />
-      <Input label='Amount' placeholder='0.00' />
       <Input
         label='Email'
         placeholder='Enter email'
