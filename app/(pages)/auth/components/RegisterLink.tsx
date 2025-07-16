dbc6237338b8597a377e794b75d1e8adeb51232d
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-16 20:51:36 +0300

    fix register link todo

--- app/(pages)/auth/components/RegisterLink.tsx
+++ app/(pages)/auth/components/RegisterLink.tsx
@@ -1,12 +1,7 @@
 export const RegisterLink = () => (
-  // TODO: Uncomment the below line to see the effect of top margin on the page.
-  // ! Never use mt-x as it caused scroll, instead manage the spacing in the parent component using
-  // ! space-y-x and gap-x
-  // <div className="mt-6 text-center">
-  <p className="text-sm text-gray-600 text-center">
-    {/* ! TODO: Remove `{"  "}`, be carful not to add it, use padding instead */}
-    Don't Have An Account? {/* ! TODO: Replace text-[#xxxxxx] */}
-    <a href="#" className="text-[#5879DC] font-medium">
+  <p className='text-sm text-gray-600 text-center'>
+    Don't Have An Account?
+    <a href='#' className='text-unitedBlue font-medium'>
       Register
     </a>
   </p>
