1765feafdc6756d4f536ebb94d1105c5055d303e
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-17 18:55:08 +0300

    add login link, register from, and register tab components

---
+++ app/(pages)/auth/components/RegisterForm.tsx
@@ -0,0 +1,20 @@
+import { Button, Input } from '@components';
+
+export const RegisterForm = () => (
+  <>
+    <div className='grid grid-cols-1 sm:grid-cols-2 gap-3'>
+      <Input label='First Name' type='text' placeholder='Enter first name' />
+      <Input label='Last Name' type='text' placeholder='Enter last name' />
+    </div>
+    <Input label='Email' type='email' placeholder='Enter email address' />
+    <Input label='Password' type='password' placeholder='Enter Password' />
+    <Input
+      label='Confirm Password'
+      type='password'
+      placeholder='Enter Password'
+    />
+    <Button variant='primary' className='w-full rounded-xl' size='large'>
+      Login
+    </Button>
+  </>
+);
---
+++ app/(pages)/auth/components/RegisterTab.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from '@components';
+
+export const RegisterTab = () => {
+  const RegisterTabs = [
+    {
+      title: 'Register as contributor',
+      link: '/auth/register/#1',
+    },
+    {
+      title: 'Register as junior',
+      link: '/auth/register/#2',
+    },
+  ];
+
+  return (
+    <>
+      <Tabs
+        className='flex items-center justify-center'
+        tabItems={RegisterTabs}
+      />
+    </>
+  );
+};
