5f84d000a91415464dec56191f11a76380413188
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-17 18:55:59 +0300

    update register page

--- app/(pages)/auth/@slot/register/page.tsx
+++ app/(pages)/auth/@slot/register/page.tsx
@@ -1,8 +1,22 @@
+import {
+  LoginLink,
+  Logo,
+  RegisterForm,
+  RegisterTab,
+  SocialLoginButtons,
+  WelcomeMessage,
+} from '../../components';
+
 const RegiserSlot = () => {
   return (
-    <>
-      <h1>Register Slot</h1>
-    </>
+    <div className='space-y-6 px-6'>
+      <Logo />
+      <WelcomeMessage content='Create an Account' />
+      <RegisterTab />
+      <SocialLoginButtons />
+      <RegisterForm />
+      <LoginLink />
+    </div>
   );
 };
 
