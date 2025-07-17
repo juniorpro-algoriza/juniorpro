2f6f3cebdb6c1c3531cf12e54d6c8e3948374c09
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-17 18:55:46 +0300

    export new components

--- app/(pages)/auth/components/index.ts
+++ app/(pages)/auth/components/index.ts
@@ -1,6 +1,10 @@
-export { Logo } from "./Logo";
-export { SocialLoginButtons } from "./SocialLoginButtons";
-export { WelcomeMessage } from "./WelcomeMessage";
-export { RegisterLink } from "./RegisterLink";
-export { LoginForm } from "./LoginForm";
-export { HeroImage } from "./HeroImage";
+export { HeroImage } from './HeroImage';
+export { LoginForm } from './LoginForm';
+export { LoginLink } from './LoginLink';
+export { Logo } from './Logo';
+export { RegisterForm } from './RegisterForm';
+export { RegisterLink } from './RegisterLink';
+export { RegisterTab } from './RegisterTab';
+export { SocialLoginButtons } from './SocialLoginButtons';
+export { WelcomeMessage } from './WelcomeMessage';
+
