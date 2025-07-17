806dac59b243f60e55c30a08228bff45a2c3d60a
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-17 18:55:31 +0300

    update social login buttons component

--- app/(pages)/auth/components/SocialLoginButtons.tsx
+++ app/(pages)/auth/components/SocialLoginButtons.tsx
@@ -3,7 +3,7 @@ import { AppleIcon, GoogleIcon } from '@icons';
 
 export const SocialLoginButtons = () => (
   <>
-    <div className='grid grid-cols-2 gap-4'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
       <Button variant='tertiary' icon={<GoogleIcon />}>
         Login With Google
       </Button>
