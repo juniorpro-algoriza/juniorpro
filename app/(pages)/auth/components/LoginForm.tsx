76e4613cbbaf30c9af57d96b8074bf85689de2d1
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-07-21 17:16:47 +0300

    add cva

--- app/(pages)/auth/components/LoginForm.tsx
+++ app/(pages)/auth/components/LoginForm.tsx
@@ -1,18 +1,18 @@
-import { Button, Input } from '@components';
+import { Button, Input } from "@components";
 
 export const LoginForm = () => (
-  <>
-    <Input label='Email' type='email' placeholder='Enter email address' />
+  <form>
+    <Input label="Email" type="email" placeholder="Enter email address" />
     <div>
-      <Input label='Password' type='password' placeholder='Enter Password' />
-      <div className='text-right pt-2'>
-        <a href='#' className='text-sm text-cadetGray font-medium'>
+      <Input label="Password" type="password" placeholder="Enter Password" />
+      <div className="text-right pt-2">
+        <a href="#" className="text-sm text-cadetGray font-medium">
           Forget Password?
         </a>
       </div>
     </div>
-    <Button variant='primary' className='w-full rounded-xl' size='large'>
+    <Button variant="primary" className="w-full rounded-xl" size="large">
       Login
     </Button>
-  </>
+  </form>
 );
