546604f474adf086cef86fe739ac060254f9d5c9
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-23 19:10:34 +0300

    update project tabs component

--- app/(pages)/home/components/ProjectsTabs.tsx
+++ app/(pages)/home/components/ProjectsTabs.tsx
@@ -1,11 +1,15 @@
-import { Tabs } from "@components";
-import { getProjectTabs } from "../server";
+import { Tabs } from '@components';
+import { getProjectTabs } from '../server';
+import { ProjectsHeader } from './ProjectsHeader';
 
 export const ProjectsTabs = async () => {
   const tabItems = await getProjectTabs();
   return (
-    <div className="pt-20">
-      <Tabs tabItems={tabItems} />
-    </div>
+    <>
+      <ProjectsHeader />
+      <div className='pt-10 flex items-center justify-center bg-white'>
+        <Tabs tabItems={tabItems} />
+      </div>
+    </>
   );
 };
