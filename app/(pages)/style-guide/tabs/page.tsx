522dd3d23b9d39a013417a14621b3cc2721f632e
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-07-24 01:02:15 +0300

    Adjust register page

--- app/(pages)/style-guide/tabs/page.tsx
+++ app/(pages)/style-guide/tabs/page.tsx
@@ -1,22 +1,16 @@
-import { Tabs } from "@components";
-
 const TabsStylePage = () => {
-  const authTabs = [
-    {
-      title: "Beta",
-      link: "#!",
-    },
-    {
-      title: "Alpha",
-      link: "#2",
-    },
-  ];
+  // const authTabs = [
+  //   {
+  //     title: "Beta",
+  //     link: "#!",
+  //   },
+  //   {
+  //     title: "Alpha",
+  //     link: "#2",
+  //   },
+  // ];
 
-  return (
-    <>
-      <Tabs tabItems={authTabs} />
-    </>
-  );
+  return <>{/* <Tabs tabItems={authTabs} /> */}</>;
 };
 
 export default TabsStylePage;
