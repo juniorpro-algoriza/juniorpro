dc25d801f1b20325dd1451d325ed525d95dde052
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-23 19:10:47 +0300

    add projects header component

---
+++ app/(pages)/home/components/ProjectsHeader.tsx
@@ -0,0 +1,12 @@
+export const ProjectsHeader = () => {
+  return (
+    <div className='text-center bg-white'>
+      <h2 className='font-medium text-[32px] text-gray-800 mb-3'>
+        Featured Projects
+      </h2>
+      <p className='text-xl text-stone-500 font-medium'>
+        Discover our most popular tech projects handpicked by experts
+      </p>
+    </div>
+  );
+};
