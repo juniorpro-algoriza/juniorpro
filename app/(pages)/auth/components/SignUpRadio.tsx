24a77c2ef2da5646b11ee5d77a2e8a1b10ebd63b
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-24 14:43:10 +0300

    updats sign up radio component

--- app/(pages)/auth/components/SignUpRadio.tsx
+++ app/(pages)/auth/components/SignUpRadio.tsx
@@ -1,20 +1,26 @@
-// TODO: Style radio groups
-// ! Add style to /app/styles/index.ts
-import { Field, Fieldset, Radio, RadioGroup } from "@headlessui/react";
+import { Field, Fieldset, Radio, RadioGroup } from '@headlessui/react';
+import { radioButton, radioFieldset } from '@styles';
 
 export const SignUpRadio = () => {
   const options = [
-    { title: "Register as contributor", value: "contributor" },
-    { title: "Register as junior", value: "junior" },
+    { title: 'Register as contributor', value: 'contributor' },
+    { title: 'Register as junior', value: 'junior' },
   ];
+
   return (
     <>
-      <RadioGroup name="signInAs" defaultValue={options[0].value}>
-        <Fieldset>
+      <RadioGroup
+        name='signInAs'
+        defaultValue={options[0].value}
+        className='w-full flex items-center justify-center'
+      >
+        <Fieldset className={radioFieldset}>
           {options.map(({ title, value }) => {
             return (
               <Field key={value}>
-                <Radio value={value}>{title}</Radio>
+                <Radio value={value} className={radioButton}>
+                  {title}
+                </Radio>
               </Field>
             );
           })}
