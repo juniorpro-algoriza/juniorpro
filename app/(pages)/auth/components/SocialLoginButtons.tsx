222c8ab796eae098e9cd5eebe65383d8df9510a6
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-24 14:36:09 +0300

    update social login buttons

--- app/(pages)/auth/components/SocialLoginButtons.tsx
+++ app/(pages)/auth/components/SocialLoginButtons.tsx
@@ -6,14 +6,16 @@ export const SocialLoginButtons = () => (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
       <Button
         intent="tertiary"
-        className="text-storm-500 rounded-xl px-14 border-border-secondary"
+        className="text-storm-500 rounded-xl px-14 border-border-secondary shadow"
+        iconPosition='left'
         icon={<GoogleIcon />}
       >
         Login With Google
       </Button>
       <Button
-        className="text-storm-500 px-14 rounded-xl border-border-secondary"
+        className="text-storm-500 px-14 rounded-xl border-border-secondary shadow"
         intent="tertiary"
+        iconPosition='left'
         icon={<AppleIcon />}
       >
         Login With Apple
@@ -22,7 +24,7 @@ export const SocialLoginButtons = () => (
 
     <div className="flex items-center">
       <div className="w-full border-t border-gray-300"></div>
-      <span className="text-sm px-2 bg-gray-50 text-gray-500">OR</span>
+      <span className="text-sm px-2 text-gray-500">OR</span>
       <div className="w-full border-t border-gray-300"></div>
     </div>
   </>
