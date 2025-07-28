29b0b2c116461fcce0a503e395942cd6e50acf42
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-28 16:41:09 +0300

    add points page

---
+++ app/(pages)/points/page.tsx
@@ -0,0 +1,8 @@
+const PointsPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-2xl font-bold'>Points Page</h1>
+    </div>
+  );
+};
+export default PointsPage;
