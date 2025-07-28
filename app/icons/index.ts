89a46d0bfd8e266e97fb643f449a481d6a2fc175
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-28 16:40:07 +0300

    add icons

--- app/icons/index.ts
+++ app/icons/index.ts
@@ -1,5 +1,12 @@
-export { AmazonIcon } from "./AmazonIcon";
-export { AppleIcon } from "./AppleIcon";
-export { GoogleIcon } from "./GoogleIcon";
-export { MetaIcon } from "./MetaIcon";
-export { MicrosoftIcon } from "./MicrosoftIcon";
+export { AmazonIcon } from './AmazonIcon';
+export { AppleIcon } from './AppleIcon';
+export { DiamondIcon } from './DiamondIcon';
+export { DocumentIcon } from './DocumentIcon';
+export { GoogleIcon } from './GoogleIcon';
+export { HomeIcon } from './HomeIcon';
+export { LogoutIcon } from './LogoutIcon';
+export { MetaIcon } from './MetaIcon';
+export { MicrosoftIcon } from './MicrosoftIcon';
+export { SettingsIcon } from './SettingsIcon';
+export { UserIcon } from './UserIcon';
+
