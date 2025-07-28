edd288d58cc508c9bfa157b921edc6f17fb339c4
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-28 16:41:18 +0300

    add profile page

---
+++ app/(pages)/profile/page.tsx
@@ -0,0 +1,8 @@
+const ProfilePage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-2xl font-bold'>Profile Page</h1>
+    </div>
+  );
+};
+export default ProfilePage;
