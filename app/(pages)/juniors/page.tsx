7e88f1cc8dc7df25dcd8f3f69d502d75f52ee56b
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-30 21:09:43 +0300

    add juniors page for contributor panel

--- app/(pages)/juniors/page.tsx
+++ app/(pages)/juniors/page.tsx
@@ -1,7 +1,22 @@
-const JuniorsPage = () => {
+import { getJuniorsAge, getJuniorsData, getJuniorsGrades } from '@server';
+import { JuniorsHeader } from './components';
+import { JuniorsTable } from './components/JuniorsTable';
+
+const JuniorsPage = async () => {
+  const juniorsData = await getJuniorsData();
+  const juniorsAge = await getJuniorsAge();
+  const juniorsGradeLevel = await getJuniorsGrades();
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-2xl font-bold'>Juniors Page</h1>
+    <div className='min-h-screen p-6 bg-stone-50'>
+      {/* Header */}
+      <JuniorsHeader />
+
+      {/* Juniors Table */}
+      <JuniorsTable
+        juniorsData={juniorsData}
+        juniorsAge={juniorsAge}
+        juniorsGrade={juniorsGradeLevel}
+      />
     </div>
   );
 };
