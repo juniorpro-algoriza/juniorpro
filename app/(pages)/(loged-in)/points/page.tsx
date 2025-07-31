1161be9996b39a0494315d8aca5d9c792cc1ab0d
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-31 22:59:01 +0300

    update points page of contributre panel

--- app/(pages)/(loged-in)/points/page.tsx
+++ app/(pages)/(loged-in)/points/page.tsx
@@ -1,7 +1,15 @@
+import {
+    PointsAllocationSection,
+    PointsHeader,
+    PurchasePoints,
+} from './components';
+
 const PointsPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-2xl font-bold'>Points Page</h1>
+    <div className='space-y-4 px-6 py-3 bg-stone-50'>
+      <PointsHeader />
+      <PointsAllocationSection />
+      <PurchasePoints />
     </div>
   );
 };
