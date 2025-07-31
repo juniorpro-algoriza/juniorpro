4b2745712709c33aceabf6e9295d72f1e34e8ab9
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-31 22:58:02 +0300

    add purchase, transaction cards components

---
+++ app/(pages)/(loged-in)/points/components/PurchasePoints.tsx
@@ -0,0 +1,49 @@
+import { Tabs } from '@components/client';
+import { TabData } from '../../../../components/types';
+import { getPointsPlans, getPointsTransactions } from '../server';
+import { PurchaseCard } from './PurchaseCard';
+import { TransactionCard } from './TransactionCard';
+
+export const PurchasePoints = async () => {
+  const plansData = await getPointsPlans();
+  const transactionsData = await getPointsTransactions();
+
+  const tabsData: TabData[] = [
+    {
+      name: 'Purchase Points',
+      content: (
+        <>
+          <h2 className='text-2xl font-medium text-yankees-blue'>
+            Purchase Points
+          </h2>
+          <div className='grid md:grid-cols-3 gap-6'>
+            {plansData.map((plan) => (
+              <PurchaseCard key={plan.id} plan={plan} />
+            ))}
+          </div>
+        </>
+      ),
+    },
+    {
+      name: 'Transactions',
+      content: (
+        <>
+          <h2 className='text-2xl font-medium text-yankees-blue'>
+            Recent Transactions
+          </h2>
+          <div className='space-y-4'>
+            {transactionsData.map((transaction) => (
+              <TransactionCard key={transaction.id} transaction={transaction} />
+            ))}
+          </div>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <Tabs tabs={tabsData} defaultSelectedIndex={0} />
+    </div>
+  );
+};
---
+++ app/(pages)/(loged-in)/points/components/TransactionCard.tsx
@@ -0,0 +1,73 @@
+import { DiamondIcon } from '@icons';
+import { CalendarIcon } from 'lucide-react';
+import { Transaction } from '../types';
+
+interface TransactionCardProps {
+  transaction: Transaction;
+}
+
+export const TransactionCard = ({ transaction }: TransactionCardProps) => {
+  return (
+    <div
+      key={transaction.id}
+      className='bg-white rounded-xl p-6 border border-antiflash-white shadow transition-shadow duration-200'
+    >
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center space-x-4'>
+          <div
+            className={`p-2 rounded-lg ${
+              transaction.type === 'purchase' ? 'bg-green-100' : 'bg-blue-100'
+            }`}
+          >
+            {transaction.icon}
+          </div>
+
+          <div>
+            <h3 className='font-medium text-dark-electric-blue'>
+              {transaction.title}
+            </h3>
+            <div className='flex items-center space-x-4 mt-1 font-light'>
+              <div className='flex items-center text-sm text-dark-electric-blue'>
+                <CalendarIcon className='w-4 h-4 mr-1' />
+                Due: {transaction.date}
+              </div>
+              <div className='flex items-center text-sm text-dark-electric-blue'>
+                <CalendarIcon className='w-4 h-4 mr-1' />
+                Due: {transaction.dueDate}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-2 items-end'>
+          {transaction.status === 'Completed' && (
+            <span className='px-3 py-1 bg-success-100 text-success-400 text-sm font-medium rounded-full'>
+              Completed
+            </span>
+          )}
+
+          <div className='text-right'>
+            {transaction.type === 'purchase' ? (
+              <div>
+                <div className='flex items-center text-success-400 font-medium space-x-1'>
+                  <span>
+                    <DiamondIcon fill='#41C980' />
+                  </span>
+                  +{transaction.points} Points
+                  <span className='ml-2 text-storm-500'>
+                    -{transaction.pointsUsed}
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <div className='text-violet-normal font-medium flex gap-1'>
+                <DiamondIcon fill='#5879dc' /> Allocated {transaction.points}{' '}
+                Points
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
