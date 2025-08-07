5a63bb82097f355b943ac7f43af7bdfdfd6c8225
alyadelaly <aliadelhacker123@yahoo.com>
2025-08-07 13:45:46 +0300

    remove provider from sidebar

--- app/(pages)/(loged-in)/layout.tsx
+++ app/(pages)/(loged-in)/layout.tsx
@@ -1,22 +1,12 @@
-import { Provider } from 'jotai';
 import type { ReactNode } from 'react';
-import {
-  SidebarLayout,
-  SidebarProvider,
-} from '../../components/client/Sidebar';
+import { SidebarLayout } from '../../components/client/Sidebar';
 
 interface LoggedInLayoutProps {
   children: ReactNode;
 }
 
 const LoggedInLayout = ({ children }: Readonly<LoggedInLayoutProps>) => {
-  return (
-    <Provider>
-      <SidebarProvider>
-        <SidebarLayout>{children}</SidebarLayout>
-      </SidebarProvider>
-    </Provider>
-  );
+  return <SidebarLayout>{children}</SidebarLayout>;
 };
 
 export default LoggedInLayout;
