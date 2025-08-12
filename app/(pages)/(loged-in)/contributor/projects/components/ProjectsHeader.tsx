8ee2203e21c86e77ebe3c76318c2ead32054251f
alyadelaly <aliadelhacker123@yahoo.com>
2025-08-12 22:04:32 +0300

    add dashboard in breadcrumb in projects header component

--- app/(pages)/(loged-in)/contributor/projects/components/ProjectsHeader.tsx
+++ app/(pages)/(loged-in)/contributor/projects/components/ProjectsHeader.tsx
@@ -3,5 +3,10 @@
 import { LoggedInPageHeader } from '../../../components/client';
 
 export const ProjectsHeader = () => {
-  return <LoggedInPageHeader title='Projects' breadcrumbs={['Projects']} />;
+  return (
+    <LoggedInPageHeader
+      title='Projects'
+      breadcrumbs={['Dashboard', 'Projects']}
+    />
+  );
 };
