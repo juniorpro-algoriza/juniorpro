ba0dbad033d946c364bd32bd205773affd755672
lina <linamohey15@Outlook.com>
2025-08-26 14:14:16 +0300

    remove commented code.

--- app/(pages)/(loged-in)/admin/project-managers/components/ProjectManagerTable.tsx
+++ app/(pages)/(loged-in)/admin/project-managers/components/ProjectManagerTable.tsx
@@ -1,11 +1,11 @@
-import { Table } from '@components';
-import { EyeIcon } from 'lucide-react';
-import { Badge } from '../../../../../components/Badge';
-import { ProjectManager } from '../../types';
+import { Table } from "@components";
+import { EyeIcon } from "lucide-react";
+import { Badge } from "../../../../../components/Badge";
+import { ProjectManager } from "../../types";
 
 interface ProjectManagerTableProps {
   projectManagerData: ProjectManager[];
-  view?: 'dashboard' | 'full';
+  view?: "dashboard" | "full";
 }
 
 export const ProjectManagerTable = ({
@@ -16,64 +16,45 @@ export const ProjectManagerTable = ({
     status: (
       <Badge
         label={projectManager.status}
-        variant={projectManager.status === 'active' ? 'green' : 'orange'}
+        variant={projectManager.status === "active" ? "green" : "orange"}
       />
     ),
   }));
 
   const columns = [
-    { header: 'Name', key: 'name' },
-    { header: 'Email', key: 'email' },
-    { header: 'Status', key: 'status' },
-    { header: 'Projects', key: 'projects' },
-    { header: 'Practice Content', key: 'practiceContent' },
-    { header: 'Contributors', key: 'contributors' },
-    { header: 'Joined On', key: 'joinedOn' },
+    { header: "Name", key: "name" },
+    { header: "Email", key: "email" },
+    { header: "Status", key: "status" },
+    { header: "Projects", key: "projects" },
+    { header: "Practice Content", key: "practiceContent" },
+    { header: "Contributors", key: "contributors" },
+    { header: "Joined On", key: "joinedOn" },
     {
-      header: 'Action',
-      key: 'action',
+      header: "Action",
+      key: "action",
       isAction: true,
-      actionLabel: 'View',
+      actionLabel: "View",
       actionIcon: <EyeIcon size={16} />,
-      href: '/admin/project-managers/id',
-      width: 'w-24',
+      href: "/admin/project-managers/id",
+      width: "w-24",
     },
   ];
 
   return (
     <div>
-      <div className='bg-white rounded-[20px] drop-shadow-xl border border-border-primary'>
-        <div className='p-6'>
-          <div className='flex items-center justify-between'>
-            <h3 className='text-xl font-medium text-yankees-blue'>
+      <div className="bg-white rounded-[20px] drop-shadow-xl border border-border-primary">
+        <div className="p-6">
+          <div className="flex items-center justify-between">
+            <h3 className="text-xl font-medium text-yankees-blue">
               Project Managers ({projectManagerData.length})
             </h3>
-            {/* <div className="flex justify-center gap-2.5">
-              <ModalLink name="AddJunior">
-                <Button intent="primary" className="text-sm" size="large">
-                  Add Contributor
-                </Button>
-              </ModalLink>
-
-              <Input
-                placeholder="Search for Contributors"
-                leftIcon={<SearchIcon size={20} />}
-                className="shadow-sm"
-              />
-              <Button
-                intent="unset"
-                className="shadow-sm px-2.5 border border-border-primary"
-              >
-                <ListFilter className="text-cadetGray" />
-              </Button>
-            </div> */}
           </div>
         </div>
 
         <Table
           columns={columns}
           data={transformedData}
-          emptyMessage='No contributor added yet'
+          emptyMessage="No contributor added yet"
         />
       </div>
     </div>
