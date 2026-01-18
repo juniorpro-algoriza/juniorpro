df3f56a717fc52edbbbaa4542f9b85baee44f8ec
ahmed246666 <aoe24.business@gmail.com>
2026-01-18 17:39:14 +0200

    fix :build

--- app/(pages)/(logged-out)/collaboration/[id]/page.tsx
+++ app/(pages)/(logged-out)/collaboration/[id]/page.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { notFound } from "next/navigation";
 import { TEAMS } from "../_data/teams";
-import { ChallengeDetails } from "../_components/ChallengeDetails";
+import { CollaborationDetails } from "../_components/CollaborationDetails";
 
 type PageProps = {
   params: Promise<{ id: string }>;
 };
 
-const ChallengeDetailsPage = async ({ params }: PageProps) => {
+const CollaborationDetailsPage = async ({ params }: PageProps) => {
   const { id } = await params;
   const team = TEAMS.find((t) => t.id === id);
 
   if (!team) notFound();
 
-  return <ChallengeDetails team={team} />;
+  return <CollaborationDetails team={team} />;
 };
 
-export default ChallengeDetailsPage;
+export default CollaborationDetailsPage;
